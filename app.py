b44fd101c129cc5110091ef18d1ebdd70e30fa32
mikasiddiqui <mikasiddiqui@gmail.com>
2023-09-09 01:19:44 +1000

    Added HF model

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/models")
+def models():
+    return render_template("models.html")
+
+@app.route("/models/hf")
+def hf():
+    return render_template("huggingface.html")
